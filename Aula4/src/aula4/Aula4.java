9816344c98c7fd4927c7227906c02881f8c131e8
Rederson19 <141458106+Rlmr1999@users.noreply.github.com>
2023-08-14 20:26:14 -0300

    Add files via upload

---
+++ Aula4/src/aula4/Aula4.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula4;
+
+/**
+ *
+ * @author 14930128609
+ */
+public class Aula4 {
+     
+    public static void main(String[] args) {
+       int a =10;
+         
+       String A = "10";
+            int c =2;
+            
+    
+     
+     int resultadoB = a - c;
+     int resultadoC = a * c;
+     int resultadoD = a / c;
+     int resultadoE = a / c;
+     
+    Systen.Out.println (ResultadoA)
+             }
+}
+
