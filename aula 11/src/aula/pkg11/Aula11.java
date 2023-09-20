4c71c1dfbae38eb414d9b31b92c4c61653a365ba
Rederson19 <141458106+Rlmr1999@users.noreply.github.com>
2023-09-20 19:16:52 -0300

    Add files via upload

---
+++ aula 11/src/aula/pkg11/Aula11.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula.pkg11;
+
+/**
+ *
+ * @author 14930128609
+ */
+public class Aula11 {
+    public static void main(String[] args) {
+  int contador = 0;
+        while (contador < 10){
+            System.out.println(contador);
+            contador++;
+                    
+        }
+        
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }                    
+     }   
+}
+        
+            
+
+    
+
---
+++ aula 11/src/aula/pkg11/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula.pkg11;
+
+/**
+ *
+ * @author 14930128609
+ */
+public class NewClass {
+    
+}
